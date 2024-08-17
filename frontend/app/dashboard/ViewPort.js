2876758e0320a743783c79f29f120826c81896a5
Aseef Durrani <aseef.durrani11@gmail.com>
2024-08-17 14:39:33 -0400

    bracket fix

--- frontend/app/dashboard/ViewPort.js
+++ frontend/app/dashboard/ViewPort.js
@@ -26,117 +26,112 @@ export default function ViewPort() {
     } catch (e) {
       console.log("ERROR: ", e);
     }
-
-    /*
+  };
+  /*
         - Obejctive: Based on user input on prompt textarea, set state 'prompt' as current value
     */
-    const handlePromptChange = (e) => {
-      const { value } = e.target;
-      setPrompt(value);
-    };
+  const handlePromptChange = (e) => {
+    const { value } = e.target;
+    setPrompt(value);
+  };
 
-    const startRecording = () => {
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then((stream) => {
-          const mediaRecorder = new MediaRecorder(stream);
-          mediaRecorderRef.current = mediaRecorder;
-          mediaRecorder.start();
-          setIsRecording(true);
+  const startRecording = () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorderRef.current = mediaRecorder;
+        mediaRecorder.start();
+        setIsRecording(true);
 
-          mediaRecorder.ondataavailable = (e) => {
-            setAudioChunks((prev) => [...prev, e.data]);
-          };
+        mediaRecorder.ondataavailable = (e) => {
+          setAudioChunks((prev) => [...prev, e.data]);
+        };
 
-          mediaRecorder.onstop = () => {
-            const blob = new Blob(audioChunks, { type: "audio/webm" });
-            setAudioFile(blob);
-            setAudioChunks([]);
-            setIsRecording(false);
-          };
-        })
-        .catch((err) => console.log("ERROR: ", err));
-    };
+        mediaRecorder.onstop = () => {
+          const blob = new Blob(audioChunks, { type: "audio/webm" });
+          setAudioFile(blob);
+          setAudioChunks([]);
+          setIsRecording(false);
+        };
+      })
+      .catch((err) => console.log("ERROR: ", err));
+  };
 
-    const stopRecording = () => {
-      if (mediaRecorderRef.current) {
-        mediaRecorderRef.current.stop();
-      }
-    };
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
+  };
 
-    const handleAudioSubmit = async () => {
-      try {
-        const formData = new FormData();
-        const audioBlob = new Blob(audioChunks, { type: "audio/webm" }); // Convert to audio/wav
-        formData.append("audioFile", audioBlob);
+  const handleAudioSubmit = async () => {
+    try {
+      const formData = new FormData();
+      const audioBlob = new Blob(audioChunks, { type: "audio/webm" }); // Convert to audio/wav
+      formData.append("audioFile", audioBlob);
 
-        const response = await axios.post(
-          "http://127.0.0.1:5000/api/audioQuery",
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        console.log("Audio file submitted:", response.data);
-      } catch (e) {
-        console.log("ERROR: ", e);
-      }
-    };
+      const response = await axios.post(
+        "http://127.0.0.1:5000/api/audioQuery",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("Audio file submitted:", response.data);
+    } catch (e) {
+      console.log("ERROR: ", e);
+    }
+  };
 
-    return (
-      <div className={styles.ViewPort}>
-        <div>
-          <h2>Credit Card</h2>
-          <div className={styles.ViewPort_Prompt}>
-            <input type="text" value={prompt} onChange={handlePromptChange} />
-            <button onClick={handlePromptSubmit}>Submit</button>
-          </div>
-        </div>
-        <div className={styles.ViewPort_List}>
-          <div className={styles.ViewPort_List_Title}>
-            <h3>List of Complaints</h3>
-          </div>
-          <div className={styles.ViewPort_List_Content}>
-            <div className={styles.ViewPort_List_Content_Tab}>
-              <p className={styles.ViewPort_List_Content_ID}>ID</p>
-              <p className={styles.ViewPort_List_Content_Product}>Product</p>
-              <p className={styles.ViewPort_List_Content_Sub_Product}>
-                Sub-Product
-              </p>
-            </div>
-            {complaints.map((complaint, idx) => (
-              <div
-                key={idx}
-                className={
-                  styles.ViewPort_List_Content_Tab +
-                  " " +
-                  styles.ViewPort_List_Content_Tab_Complaints
-                }
-              >
-                <p className={styles.ViewPort_List_Content_ID}>
-                  {complaint.id}
-                </p>
-                <p className={styles.ViewPort_List_Content_Product}>
-                  {complaint.product}
-                </p>
-                <p className={styles.ViewPort_List_Content_Sub_Product}>
-                  {complaint.sub_product}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div>
-          <button onClick={isRecording ? stopRecording : startRecording}>
-            {isRecording ? "Stop Recording" : "Start Recording"}
-          </button>
-          {audioFile && (
-            <button onClick={handleAudioSubmit}>Submit Audio</button>
-          )}
+  return (
+    <div className={styles.ViewPort}>
+      <div>
+        <h2>Credit Card</h2>
+        <div className={styles.ViewPort_Prompt}>
+          <input type="text" value={prompt} onChange={handlePromptChange} />
+          <button onClick={handlePromptSubmit}>Submit</button>
         </div>
       </div>
-    );
-  };
+      <div className={styles.ViewPort_List}>
+        <div className={styles.ViewPort_List_Title}>
+          <h3>List of Complaints</h3>
+        </div>
+        <div className={styles.ViewPort_List_Content}>
+          <div className={styles.ViewPort_List_Content_Tab}>
+            <p className={styles.ViewPort_List_Content_ID}>ID</p>
+            <p className={styles.ViewPort_List_Content_Product}>Product</p>
+            <p className={styles.ViewPort_List_Content_Sub_Product}>
+              Sub-Product
+            </p>
+          </div>
+          {complaints.map((complaint, idx) => (
+            <div
+              key={idx}
+              className={
+                styles.ViewPort_List_Content_Tab +
+                " " +
+                styles.ViewPort_List_Content_Tab_Complaints
+              }
+            >
+              <p className={styles.ViewPort_List_Content_ID}>{complaint.id}</p>
+              <p className={styles.ViewPort_List_Content_Product}>
+                {complaint.product}
+              </p>
+              <p className={styles.ViewPort_List_Content_Sub_Product}>
+                {complaint.sub_product}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <button onClick={isRecording ? stopRecording : startRecording}>
+          {isRecording ? "Stop Recording" : "Start Recording"}
+        </button>
+        {audioFile && <button onClick={handleAudioSubmit}>Submit Audio</button>}
+      </div>
+    </div>
+  );
 }
